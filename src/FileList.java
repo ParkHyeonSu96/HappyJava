e6fd67c9a363ca0a5abc6442be76417795002da2
ParkHyeonSu <hyunsuddong@naver.com>
2025-01-17 17:11:08 +0900

    FileList commit

---
+++ src/FileList.java
@@ -0,0 +1,24 @@
+import java.io.File;
+
+public class FileList {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\user\\Desktop\\ParkHyeonSu");
+
+        printFiles(file);
+
+    }
+
+    private static void printFiles(File file) {
+        if(file.isDirectory()) {
+            File[] files = file.listFiles();
+            for(int i = 0; i < files.length; i++){
+                System.out.println(files[i].getName());
+                printFiles(files[i]);
+            }
+        }else if(file.isFile()){
+            System.out.println(file.getName());
+        }else {
+            System.out.println("else --" + file.getName());
+        }
+    }
+}
