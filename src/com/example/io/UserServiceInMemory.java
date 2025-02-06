a6d0ca0a2ee71cc6966353356c238b495d45ebfd
ParkHyeonSu <hyunsuddong@naver.com>
2025-02-06 21:49:46 +0900

    User Management Project update commit3

---
+++ src/com/example/io/UserServiceInMemory.java
@@ -0,0 +1,26 @@
+package com.example.io;
+
+import java.util.List;
+
+// 메모리상에 User정보를 관리하는 클래스
+public class UserServiceInMemory implements UserService {
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public void deleteUser(String email) {
+
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return List.of();
+    }
+}
